e7ae90e28e0e52287ed7b692e0cdc12e88d2e028
Danny Zhang <Danny.Zhang@blackboard.com>
2018-01-18 17:54:09 +0800

    add show more animation

---
+++ app/src/main/java/com/aesean/activitystack/utils/TextViewUtils.java
@@ -0,0 +1,238 @@
+/*
+ *    Copyright (C) 2017.  Aesean
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.aesean.activitystack.utils;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.RectF;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextDirectionHeuristic;
+import android.text.TextDirectionHeuristics;
+import android.text.TextPaint;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
+import java.util.Hashtable;
+
+/**
+ * TextViewUtils.
+ * {@link android.support.v7.widget.AppCompatTextViewAutoSizeHelper}
+ *
+ * @author danny
+ * @version 1.0
+ * @since 1/17/18
+ */
+@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class TextViewUtils {
+    private static final String TAG = "TextViewUtils";
+    private static Hashtable<String, Method> sTextViewMethodByNameCache = new Hashtable<>();
+
+    /**
+     * ms
+     */
+    private static final long DURATION = 300;
+
+    // horizontal scrolling is activated.
+    private static final int VERY_WIDE = 1024 * 1024;
+    private static RectF TEMP_RECT_F = new RectF();
+    private static TextPaint sTempTextPaint;
+
+    public static void setMaxLineWithAnimation(final TextView textView, final int maxLine) {
+        int targetHeight = TextViewUtils.getTargetHeight(textView, textView.getText(), maxLine);
+        animatorToHeight(textView, targetHeight, new AnimatorListenerAdapter() {
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+            }
+
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                textView.setMinLines(0);
+                textView.setMaxLines(maxLine);
+            }
+        });
+    }
+
+    public static void animatorToHeight(final TextView textView, int h, Animator.AnimatorListener listener) {
+        final int height = textView.getHeight();
+        if (height == h) {
+            return;
+        }
+        ValueAnimator valueAnimator = new ValueAnimator();
+        valueAnimator.setIntValues(height, h);
+        valueAnimator.setDuration(DURATION);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int value = (int) animation.getAnimatedValue();
+                textView.setHeight(value);
+            }
+        });
+        valueAnimator.addListener(listener);
+        valueAnimator.start();
+    }
+
+    public static int getTargetHeight(TextView textView, CharSequence text) {
+        return getTargetHeight(textView, text, Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN ? textView.getMaxLines() : -1);
+    }
+
+    public static int getTargetHeight(TextView textView, CharSequence text, int maxLines) {
+        if (sTempTextPaint == null) {
+            sTempTextPaint = new TextPaint();
+        }
+        sTempTextPaint.set(textView.getPaint());
+        sTempTextPaint.setTextSize(textView.getTextSize());
+
+        final int targetHeight = getTargetHeight(textView, text, sTempTextPaint, maxLines);
+        sTempTextPaint.reset();
+        return targetHeight;
+    }
+
+    public static int getTargetHeight(TextView textView, CharSequence text, TextPaint textPaint, int maxLines) {
+        final boolean horizontallyScrolling = invokeAndReturnWithDefault(textView, "getHorizontallyScrolling", false);
+        final int availableWidth = horizontallyScrolling
+                ? VERY_WIDE
+                : textView.getMeasuredWidth() - textView.getTotalPaddingLeft()
+                - textView.getTotalPaddingRight();
+        final int availableHeight = textView.getHeight() - textView.getCompoundPaddingBottom()
+                - textView.getCompoundPaddingTop();
+
+        if (availableWidth <= 0 || availableHeight <= 0) {
+            return -1;
+        }
+        TEMP_RECT_F.setEmpty();
+        TEMP_RECT_F.right = availableWidth;
+        // TEMP_RECT_F.bottom = availableHeight;
+        TEMP_RECT_F.bottom = VERY_WIDE;
+
+        // Needs reflection call due to being private.
+        Layout.Alignment alignment = invokeAndReturnWithDefault(
+                textView, "getLayoutAlignment", Layout.Alignment.ALIGN_NORMAL);
+        final StaticLayout layout = createStaticLayoutForMeasuringCompat(text, alignment, Math.round(TEMP_RECT_F.right), Integer.MAX_VALUE, textPaint, textView);
+        return layout.getLineTop(Math.min(layout.getLineCount(), maxLines));
+    }
+
+    private static StaticLayout createStaticLayoutForMeasuringCompat(CharSequence text, Layout.Alignment alignment, int availableWidth, int maxLines, TextPaint textPaint, TextView textView) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return createStaticLayoutForMeasuring(text, alignment, availableWidth, maxLines, textPaint, textView);
+        } else {
+            return createStaticLayoutForMeasuringPre23(text, alignment, availableWidth, textPaint, textView);
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private static StaticLayout createStaticLayoutForMeasuring(CharSequence text, Layout.Alignment alignment, int availableWidth, int maxLines, TextPaint textPaint, TextView textView) {
+        // Can use the StaticLayout.Builder (along with TextView params added in or after
+        // API 23) to construct the layout.
+        final TextDirectionHeuristic textDirectionHeuristic = invokeAndReturnWithDefault(textView, "getTextDirectionHeuristic", TextDirectionHeuristics.FIRSTSTRONG_LTR);
+
+        final StaticLayout.Builder layoutBuilder = StaticLayout.Builder.obtain(text, 0, text.length(), textPaint, availableWidth);
+
+        return layoutBuilder.setAlignment(alignment)
+                .setLineSpacing(textView.getLineSpacingExtra(), textView.getLineSpacingMultiplier())
+                .setIncludePad(textView.getIncludeFontPadding())
+                .setBreakStrategy(textView.getBreakStrategy())
+                .setHyphenationFrequency(textView.getHyphenationFrequency())
+                .setMaxLines(maxLines == -1 ? Integer.MAX_VALUE : maxLines)
+                .setTextDirection(textDirectionHeuristic)
+                .build();
+    }
+
+    @RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private static StaticLayout createStaticLayoutForMeasuringPre23(CharSequence text, Layout.Alignment alignment, int availableWidth, TextPaint textPaint, TextView textView) {
+        // Setup defaults.
+        float lineSpacingMultiplier = 1.0f;
+        float lineSpacingAdd = 0.0f;
+        boolean includePad = true;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            // Call public methods.
+            lineSpacingMultiplier = textView.getLineSpacingMultiplier();
+            lineSpacingAdd = textView.getLineSpacingExtra();
+            includePad = textView.getIncludeFontPadding();
+        } else {
+            // Call private methods and make sure to provide fallback defaults in case something
+            // goes wrong. The default values have been inlined with the StaticLayout defaults.
+            lineSpacingMultiplier = invokeAndReturnWithDefault(textView,
+                    "getLineSpacingMultiplier", lineSpacingMultiplier);
+            lineSpacingAdd = invokeAndReturnWithDefault(textView,
+                    "getLineSpacingExtra", lineSpacingAdd);
+            //noinspection ConstantConditions
+            includePad = invokeAndReturnWithDefault(textView,
+                    "getIncludeFontPadding", includePad);
+        }
+
+        // The layout could not be constructed using the builder so fall back to the
+        // most broad constructor.
+        return new StaticLayout(text, textPaint, availableWidth,
+                alignment,
+                lineSpacingMultiplier,
+                lineSpacingAdd,
+                includePad);
+    }
+
+
+    private static <T> T invokeAndReturnWithDefault(@NonNull Object object, @NonNull final String methodName, @NonNull final T defaultValue) {
+        T result = null;
+        boolean exceptionThrown = false;
+
+        try {
+            // Cache lookup.
+            Method method = getTextViewMethod(methodName);
+            if (method != null) {
+                //noinspection unchecked
+                result = (T) method.invoke(object);
+            }
+        } catch (Exception ex) {
+            exceptionThrown = true;
+            Log.w(TAG, "Failed to invoke TextView#" + methodName + "() method", ex);
+        } finally {
+            if (result == null && exceptionThrown) {
+                result = defaultValue;
+            }
+        }
+
+        return result;
+    }
+
+    @SuppressLint("PrivateApi")
+    @Nullable
+    private static Method getTextViewMethod(@NonNull final String methodName) {
+        try {
+            Method method = sTextViewMethodByNameCache.get(methodName);
+            if (method == null) {
+                method = TextView.class.getDeclaredMethod(methodName);
+                if (method != null) {
+                    method.setAccessible(true);
+                    // Cache update.
+                    sTextViewMethodByNameCache.put(methodName, method);
+                }
+            }
+            return method;
+        } catch (Exception ex) {
+            Log.w(TAG, "Failed to retrieve TextView#" + methodName + "() method", ex);
+            return null;
+        }
+    }
+}
